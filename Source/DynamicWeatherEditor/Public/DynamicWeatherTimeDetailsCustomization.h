57b6e40f1d92edf548311976d70b6280ff10a1c9
Junghyeon0710 <sie08357@naver.com>
2025-05-15 11:47:32 +0900

    Editor Module +  , Time Custom

---
+++ Source/DynamicWeatherEditor/Public/DynamicWeatherTimeDetailsCustomization.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "IPropertyTypeCustomization.h"
+#include "Internationalization/Text.h"
+#include "Templates/SharedPointer.h"
+#include "Types/SlateEnums.h"
+
+class FDetailWidgetRow;
+class IDetailChildrenBuilder;
+class IPropertyHandle;
+
+/**
+ * 
+ */
+class DYNAMICWEATHEREDITOR_API FDynamicWeatherTimeDetailsCustomization : public IPropertyTypeCustomization
+{
+public:
+	static TSharedRef<IPropertyTypeCustomization> MakeInstance()
+	{
+		return MakeShared<FDynamicWeatherTimeDetailsCustomization>();
+	}
+
+	FDynamicWeatherTimeDetailsCustomization()
+	{}
+
+	/** IPropertyTypeCustomization interface */
+	virtual void CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle, FDetailWidgetRow& HeaderRow, IPropertyTypeCustomizationUtils& CustomizationUtils) override {}
+	virtual void CustomizeChildren(TSharedRef<IPropertyHandle> PropertyHandle, IDetailChildrenBuilder& ChildBuilder, IPropertyTypeCustomizationUtils& CustomizationUtils) override;
+
+private:
+	FText OnGetTimeText() const;
+	void OnTimeTextCommitted(const FText& InText, ETextCommit::Type CommitInfo);
+
+	/** Store the property handle to the Time field so we can get/set the value on the object via text box callbacks. */
+	TSharedPtr<IPropertyHandle> TimeProperty;
+};
