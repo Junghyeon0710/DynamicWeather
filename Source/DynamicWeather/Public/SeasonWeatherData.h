50f110d9840758697b22db7e4a32420a99003913
Junghyeon0710 <sie08357@naver.com>
2025-08-26 17:51:40 +0900

    feat: 날씨타입 None추가

--- Source/DynamicWeather/Public/SeasonWeatherData.h
+++ Source/DynamicWeather/Public/SeasonWeatherData.h
@@ -3,6 +3,7 @@
 UENUM(BlueprintType)
 enum class EWeatherType : uint8
 {
+    None       UMETA(DisplayName = "없음"),      // 날씨 영향 없음 (기본 상태)
 	Sunny      UMETA(DisplayName = "맑음"),
 	Rain       UMETA(DisplayName = "비"),
 	HeavyRain  UMETA(DisplayName = "폭우"),
